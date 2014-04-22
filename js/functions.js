d5c7c486dd526941118ff1749cf3ac329b5d6d39
raul <raul@zamacona.me>
2014-04-22 12:46:00 -0500

    servicios

--- js/functions.js
+++ js/functions.js
@@ -31,22 +31,44 @@
 		$('.home').backstretch('images/background.jpg');
 
 		//Windows
-		 $('.seccion').windows({
+		 $('section').windows({
 	        snapping: true,
-	        snapSpeed: 500,
-	        snapInterval: 1100,
+	        snapSpeed: 250,
+	        snapInterval: 500,
 	        onScroll: function(scrollPos){
 	            // scrollPos:Number
 	        },
 	        onSnapComplete: function($el){
 	            // after window ($el) snaps into place
+	            var seccion = $el.data('seccion');
+
+	            if( seccion != 'home' ){
+	            	fixMenu();
+	            } else {
+	            	unfixMenu()
+	            }
 	        },
 	        onWindowEnter: function($el){
 	            // when new window ($el) enters viewport
+	            //console.log($el);
 	        }
 	    })
-
 
 	});
 
 })(jQuery);
+
+var fixedBox = $('.fixedBox');
+
+function fixMenu (){
+
+	fixedBox.addClass('fixed');
+
+}
+
+
+function unfixMenu (){
+
+	fixedBox.removeClass('fixed');
+
+}

f210833214727ec42907602d04efdcd26c85125c
raul <raul@zamacona.me>
2015-04-16 15:14:43 -0500

    menu movil and mobile is done!

--- 1.2/js/functions.js
+++ 1.2/js/functions.js
@@ -26,7 +26,41 @@
 /*------------------------------------*\
 	#ON LOAD
 \*------------------------------------*/
+function creaMapa (){
 
+	var styles = [
+		{
+		  stylers: [
+			{ hue: "#00a8ab" }
+		  ]
+		}
+	];
+
+	function initialize() {
+		var notMobile = $(document).width() > 480 ? true : false;
+		var centro = new google.maps.LatLng(19.402319, -99.172324);
+		var nosotros = new google.maps.LatLng(19.402319, -99.172324);
+		var mapOptions = {
+			zoom: 19,
+			center: nosotros,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			styles: styles,
+			scrollwheel: false,
+			draggable: notMobile
+		}
+
+		var map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
+		var image = '../images/marker.png';
+
+		var marker = new google.maps.Marker({
+			position: nosotros,
+			map: map,
+			title: 'Pequeño Cuervo',
+			icon: image
+		});
+	}
+	google.maps.event.addDomListener(window, 'load', initialize);
+}
 
 
 
@@ -80,6 +114,11 @@ function closeModal(element){
 	$(document).unbind('keydown');
 }
 
+
+function toggleMenuMovil(){
+	$('.js-mobile-menu').toggleClass('closed');
+	$('.lines-button').toggleClass('closed');
+}
 
 
 
